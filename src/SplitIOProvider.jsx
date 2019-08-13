3341e4defb928ea0c2c5b2d67b4dd28e709888c3
kyle-ruan <kyleryk123@gmail.com>
2019-08-13 20:30:43 +1000

    params description

--- src/SplitIOProvider.jsx
+++ src/SplitIOProvider.jsx
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { SplitIOContext } from './SplitIOContext';
 import { useSplitClient } from './hooks/use-split-client';
 
+/**
+ * @param {string} splitKey                 internal user id, or the organisation id that the user belongs to
+ * @param {string} authorizationKey         your split api key
+ * @param {string[]} splits                 split names
+ * @param {Object} [splitFactory]           [optional] splitFactory instance, required if using cdn, which will expose splitio globally
+ * @param {Object} attributes               user attributes to determine split treatment value
+ */
 const SplitIOProvider = ({
   splitKey,
   authorizationKey,
-  attributes,
   splits,
+  attributes,
   splitFactory,
   children
 }) => {
