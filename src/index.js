eeefae42c9db9055b762e6899ec39e9aaf6cf007
kyle-ruan <kyleryk123@gmail.com>
2019-08-19 10:49:16 +1000

    setup CRA example

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-export * from './SplitIOProvider';
-export * from './SplitIOContext';
-export * from './hooks/use-split-treatment';
-export * from './hooks/use-split-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+export * from './SplitIOProvider';
+export * from './SplitIOContext';
+export * from './hooks/use-split-treatment';
+export * from './hooks/use-split-client';
